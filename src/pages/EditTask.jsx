6c943644dbf65b1ed75eda79cbf0eafc75159c90
taga3s <bepozurapu274@gmail.com>
2024-08-18 10:54:52 +0900

    fix

--- src/pages/EditTask.jsx
+++ src/pages/EditTask.jsx
@@ -25,7 +25,7 @@ export const EditTask = () => {
     const data = {
       title: title,
       detail: detail,
-      done: false,
+      done: isDone,
       limit: formatWithISOString(datetime),
     };
 
